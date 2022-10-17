19f95923b223dd9c1cc2a2ae99a2def0a7d51f6d
jossansik <jossansik@gmail.com>
2022-10-17 19:05:27 +0200

    Use correct baseUrl for heroku

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-axios.defaults.baseURL = 'http://localhost:8000/'
-// axios.defaults.baseURL = 'https://partysite-api.herokuapp.com/'
+// axios.defaults.baseURL = 'http://localhost:8000/'
+axios.defaults.baseURL = 'https://partysite-api.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 

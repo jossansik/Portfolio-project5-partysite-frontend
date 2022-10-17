38b23d80b7ebf6cd8b8666797b4e3644404000d3
jossansik <jossansik@gmail.com>
2022-10-17 19:05:10 +0200

    Show owner on click to verify logged in user

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const HomePage = () => {
   const currentUser = useCurrentUser();
+  const [owner, setOwner] = useState(null);
 
   const getUserInfo = async () => {
-    console.log(currentUser)
     const { data } = await axiosReq.get("api/profiles/" + currentUser.profile_id + "/");
-    console.log(data);
+    setOwner(data.owner);
   }
 
   return (
     <div>
       <h2>PREPARE FOR FUN SCARES!</h2>
-      <div><button onClick={getUserInfo}>Console.log user info</button></div>
+      <div>
+        <button onClick={getUserInfo}>Show owner</button>
+      </div>
+      {owner}
     </div>
   )
 }

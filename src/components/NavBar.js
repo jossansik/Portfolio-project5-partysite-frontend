2e099cf9c2e716c75fc08b1fa30d85b53b9abd6f
jossansik <jossansik@gmail.com>
2022-10-23 08:30:07 +0200

    Add logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from "../styles/NavBar.module.css";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from '../contexts/CurrentUserContext'
+import { axiosReq } from '../api/axiosDefaults'
+import styles from '../styles/NavBar.module.css'
+import { removeTokenTimestamp } from '../utils/utils'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+  const currentUser = useCurrentUser()
+  const setCurrentUser = useSetCurrentUser()
   const [expanded, setExpanded] = useState(false)
+
+  const handleSignOut = async () => {
+    try {
+      await axiosReq.post('dj-rest-auth/logout/', null)
+      setCurrentUser(null)
+      removeTokenTimestamp()
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <Navbar
@@ -26,14 +44,29 @@ const NavBar = () => {
             <NavLink className={styles.NavLink} to="/">
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <>
-              <NavLink className={styles.NavLink} to="/signin">
-                <i className="fas fa-sign-in-alt"></i>Sign in
-              </NavLink>
-              <NavLink to="/signup" className={styles.NavLink}>
-                <i className="fas fa-user-plus"></i>Sign up
-              </NavLink>
-            </>
+            {currentUser ? (
+              <>
+                <NavLink
+                  className={styles.NavLink}
+                  to="/"
+                  onClick={handleSignOut}
+                >
+                  <i className="fas fa-sign-out-alt"></i>Sign out
+                </NavLink>
+                <NavLink className={styles.NavLink} to={`/profiles/`}>
+                    {currentUser?.name}
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink className={styles.NavLink} to="/signin">
+                  <i className="fas fa-sign-in-alt"></i>Sign in
+                </NavLink>
+                <NavLink to="/signup" className={styles.NavLink}>
+                  <i className="fas fa-user-plus"></i>Sign up
+                </NavLink>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>

1baba23d4facb72c79bcc09e86b0c70d20ef6668
jossansik <jossansik@gmail.com>
2022-10-27 20:14:01 +0200

    Adjust styling in profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,23 +1,13 @@
 import React, { useEffect, useState } from "react";
+import Image from "react-bootstrap/Image";
+import { fetchMoreData } from "../../utils/utils";
 import { Link } from "react-router-dom";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
+import { Col, Row, Container, Button } from "react-bootstrap";
 import Asset from "../../components/Asset";
-
-import styles from "../../styles/ProfilePage.module.css";
-import appStyles from "../../App.module.css";
-
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
-import Image from "react-bootstrap/Image";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
-import { fetchMoreData } from "../../utils/utils";
-import NoResults from "../../assets/no-results.png";
-import { Button } from "react-bootstrap";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -47,11 +37,7 @@ function ProfilePage() {
     <>
       <Row className="px-3 text-center">
         <div>
-          <Image
-            className={styles.ProfileImage}
-            roundedCircle
-            src={profileData?.image}
-          />
+          <Image roundedCircle src={profileData?.image} />
         </div>
         <div>
           <h3 className="m-2">{profileData?.owner}</h3>
@@ -62,14 +48,11 @@ function ProfilePage() {
             </Col>
           </Row>
         </div>
-      {profileData?.is_owner && (
-        <Link
-          to={`/profiles/${profileData?.id}/edit`}
-        >
-        <Button className="btn btn-dark btn-lg float-end btn btn-primary">
-           Edit profile
-        </Button></Link>
-      )}
+        {profileData?.is_owner && (
+          <Link to={`/profiles/${profileData?.id}/edit`}>
+            <Button className="btn btn-dark float-end">Edit profile</Button>
+          </Link>
+        )}
       </Row>
     </>
   );
@@ -96,17 +79,16 @@ function ProfilePage() {
           next={() => fetchMoreData(profilePosts, setProfilePosts)}
         />
       ) : (
-        <Asset
-          src={NoResults}
-          message={`No results found you have not posted anything yet.`}
-        />
+        <div className="text-center">
+          No results found you have not posted anything yet.
+        </div>
       )}
     </>
   );
 
   return (
     <Col className="m-auto py-2 p-0 p-lg-2" md={6}>
-      <Container className={appStyles.Content}>
+      <Container>
         {hasLoaded ? (
           <>
             {mainProfile}
